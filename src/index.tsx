25e15213dcfeab16368d5092ac9db525bca1b22a
Jaakko <jaakko.tulkki.npm@gmail.com>
2020-11-16 19:08:56 +0000

    use only one setState()

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React, {useLayoutEffect, useState} from 'react';
 
 export function useResizeObserver(ref) {
-    const [current, setCurrent] = useState(null);
-    const [entries, setEntries] = useState(null);
+    const [observation, setObservation] = useState({current: null, entries: null});
     useLayoutEffect(() => {
         if (!ref.current) {
             return;
         }
         const observer = new ResizeObserver((entries) => {
-            setCurrent(ref.current);
-            setEntries(entries);
+            const o = {
+                current: ref.current,
+                entries,
+            }
+            setObservation(o);
         });
         observer.observe(ref.current);
         return () => observer.disconnect();
 
     }, [ref]);
-    return {current, entries};
+    return observation;
 }
